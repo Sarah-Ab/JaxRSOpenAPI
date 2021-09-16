4625e69e7aac25faa80943c63f04bbceac1bae60
jcochet <juliencochet35@gmail.com>
2021-09-16 09:56:19 +0200

    correction erreurs sur UserDao

--- src/main/java/fr/istic/taa/jaxrs/dao/UserDao.java
+++ src/main/java/fr/istic/taa/jaxrs/dao/UserDao.java
@@ -1,20 +1,20 @@
 package fr.istic.taa.jaxrs.dao;
 
+import java.util.List;
+
 import fr.istic.taa.jaxrs.dao.generic.AbstractJpaDao;
 import fr.istic.taa.jaxrs.domain.Appointment;
 import fr.istic.taa.jaxrs.domain.User;
-import fr.istic.taa.jaxrs.domain.Worker;
 
-import java.util.Collections;
-import java.util.List;
-
-public class UserDao extends AbstractJpaDao <Long, Appointment> {
-    public void create(String name, List <Appointment> appointments , String email, long id,String password){
-        User user=new User();
-        user.setName(name);
-        user.setAppointments(appointments);
-        user.setEmail(email);
-        user.setId(id);
-        user.setPassword(password);
-    }
+public class UserDao extends AbstractJpaDao<Long, User> {
+	
+	public void create(String name, List<Appointment> appointments, String email, long id, String password) {
+		User user = new User();
+		user.setName(name);
+		user.setAppointments(appointments);
+		user.setEmail(email);
+		user.setId(id);
+		user.setPassword(password);
+		save(user);
+	}
 }
