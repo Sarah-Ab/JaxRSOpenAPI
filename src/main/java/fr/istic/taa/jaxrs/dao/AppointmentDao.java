ff338fcc5b10f108d2534db2912d2d3c2daf9c66
jcochet <juliencochet35@gmail.com>
2021-09-16 09:37:18 +0200

    Ajout d'une classe AppointmentDao

---
+++ src/main/java/fr/istic/taa/jaxrs/dao/AppointmentDao.java
@@ -0,0 +1,23 @@
+package fr.istic.taa.jaxrs.dao;
+
+import java.sql.Date;
+
+import fr.istic.taa.jaxrs.dao.generic.AbstractJpaDao;
+import fr.istic.taa.jaxrs.domain.Appointment;
+import fr.istic.taa.jaxrs.domain.User;
+import fr.istic.taa.jaxrs.domain.Worker;
+
+public class AppointmentDao extends AbstractJpaDao<Long, Appointment> {
+
+	public void create(Long id, Date date, int duration, User user, Worker worker, String description) {
+		Appointment appointement = new Appointment();
+		appointement.setId(id);
+		appointement.setDate(date);
+		appointement.setDuration(duration);
+		appointement.setUser(user);
+		appointement.setWorker(worker);
+		appointement.setDescription(description);
+		save(appointement);
+	}
+
+}
