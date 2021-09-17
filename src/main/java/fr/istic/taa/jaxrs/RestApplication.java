f55cf1389f9002011d67a1c627bdcffc2f74bb13
jcochet <juliencochet35@gmail.com>
2021-09-17 09:46:57 +0200

    Ajout de swaggerui

--- src/main/java/fr/istic/taa/jaxrs/RestApplication.java
+++ src/main/java/fr/istic/taa/jaxrs/RestApplication.java
@@ -7,6 +7,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import fr.istic.taa.jaxrs.rest.AppointmentResource;
+import fr.istic.taa.jaxrs.rest.SwaggerResource;
 import fr.istic.taa.jaxrs.rest.UserResource;
 import fr.istic.taa.jaxrs.rest.WorkerResource;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
@@ -17,16 +18,19 @@ public class RestApplication extends Application {
 	@Override
 	public Set<Class<?>> getClasses() {
 		final Set<Class<?>> resources = new HashSet<>();
-		
+
 		// Swagger endpoints
 		resources.add(OpenApiResource.class);
-		
+
 		// Your own resources
 		resources.add(AppointmentResource.class);
 		resources.add(UserResource.class);
 		resources.add(WorkerResource.class);
-		
+
+		// Swagger resource
+		resources.add(SwaggerResource.class);
+
 		return resources;
 	}
-	
+
 }
